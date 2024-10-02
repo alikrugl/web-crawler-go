85c8d67c9fa6f449cc77c0d7ef3c6e87ff15ab1e
Alik Kruhlienia <alikrugl11@gmail.com>
2024-10-02 13:58:02 +0300

    get html from url command

---
+++ get_html.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func getHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("got Network error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("HTTP error: %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("got non-HTML response: %s", contentType)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	rawBaseURL := args[0]
+
+	fmt.Printf("starting crawl of: %s\n", rawBaseURL)
+
+	htmlBody, err := getHTML(rawBaseURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(htmlBody)
 }
